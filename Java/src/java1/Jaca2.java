e97c19396fb74c4ebf38a5e0864504018f415cd4
iamhrsl <harshalkhapare@gmail.com>
2022-09-20 15:57:25 +0530

    first one

---
+++ Java/src/java1/Jaca2.java
@@ -0,0 +1,15 @@
+package java1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Jaca2 {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "D:\\Automation setup\\chromedriver.exe");
+		 WebDriver driver =new ChromeDriver();
+
+	}
+
+}
